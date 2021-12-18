746dcc688895fff8cb7d5e0a00e0e1ef97cd2990
Towfik1995 <towfikahmed09@gmail.com>
2021-12-18 16:34:49 +0600

    professor kim

---
+++ EMDR/Assets/partic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class partic : MonoBehaviour
+{
+    public ParticleSystem part;
+
+
+    public void partt()
+    {
+        part.Play();
+    }
+}
---
+++ EMDR/Assets/pendulam.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pendulam : MonoBehaviour
+{
+
+    float timer=0f;
+    float speed=1f;
+    int phase = 0;
+  
+    void Update()
+    {
+        timer += Time.fixedDeltaTime;
+        if (timer > 1f)
+        {
+            phase++;
+            phase %= 4;
+            timer = 0f;
+        }
+        switch (phase)
+        {
+            case 0:
+                transform.Rotate(0f, 0f, speed * (1 - timer));
+                break;
+            case 1:
+                transform.Rotate(0f, 0f, -speed * timer);
+                break;
+            case 2:
+                transform.Rotate(0f, 0f, -speed *(1- timer));
+                break;
+            case 3:
+                transform.Rotate(0f, 0f, speed * timer);
+                break;
+
+        }
+    }
+    public void speedup()
+    {
+        timer += Time.fixedDeltaTime;
+        if (timer > 1f)
+        {
+            phase++;
+            phase %= 4;
+            timer = 0f;
+        }
+        switch (phase)
+        {
+            case 0:
+                transform.Rotate(0f, 0f, 10 * (1 - timer));
+                break;
+            case 1:
+                transform.Rotate(0f, 0f, -10 * timer);
+                break;
+            case 2:
+                transform.Rotate(0f, 0f, -10* (1 - timer));
+                break;
+            case 3:
+                transform.Rotate(0f, 0f, 10 * timer);
+                break;
+
+        }
+    }
+    public void speedupplus()
+    {
+        timer += Time.fixedDeltaTime;
+        if (timer > 1f)
+        {
+            phase++;
+            phase %= 4;
+            timer = 0f;
+        }
+        switch (phase)
+        {
+            case 0:
+                transform.Rotate(0f, 0f, 20 * (1 - timer));
+                break;
+            case 1:
+                transform.Rotate(0f, 0f, -20 * timer);
+                break;
+            case 2:
+                transform.Rotate(0f, 0f, -20 * (1 - timer));
+                break;
+            case 3:
+                transform.Rotate(0f, 0f, 20 * timer);
+                break;
+
+        }
+    }
+}
